6a1c4c83fd3ba44ceae6d42d5d73349f6a8853b7
Kevin Yeh <kevinyeah@utexas.edu>
2022-07-14 19:13:30 -0400

    cleanup

--- scripts/2b-proofing-alphanumeric.py
+++ scripts/2b-proofing-alphanumeric.py
@@ -4,8 +4,8 @@ fontpath = 'Times New Roman'
 margin = 50
 
 txt = string.ascii_lowercase + string.ascii_uppercase + string.digits
-fs = FormattedString(txt, font=fontpath, fontSize=60)
+fs = FormattedString(txt, font=fontpath, fontSize=60, align="center")
 
 while fs:
-    newPage('A4')
+    newPage('A4Landscape')
     fs = textBox(fs, (margin, margin, width() - 2*margin, height() - 2*margin))
